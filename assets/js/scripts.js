3446c20d51c5b6b4ed74ef50b9b9d42a36129f08
KathiaBernal <kathiabernal@gmail.com>
2022-08-09 20:58:43 -0600

    Se realizó como práctica un contador de números

---
+++ assets/js/scripts.js
@@ -0,0 +1,23 @@
+// Necesito que este contador haga 3 cosas
+// Incremente de uno en uno
+// Decremente de uno en uno
+// Borre toda la cuenta
+// Necesitamos iniciar en 0
+
+
+var valorContador = 0; //Iniciamos nuestra variable en 0
+
+function incrementar(){
+    valorContador++; //incrementar de 1 en 1
+    document.getElementById("contador").innerHTML = valorContador;
+}
+
+function decrementar(){
+    valorContador--;
+    document.getElementById("contador").innerHTML = valorContador;
+}
+
+function resetear(){
+    valorContador = 0;
+    document.getElementById("contador").innerHTML = valorContador;
+}
